fcbaa0c015df232fd01019c0171ac5a2d105baa9
nehathumu <nthumu@haverford.edu>
2021-03-06 00:05:49 -0500

    flix pt 2

--- app/src/main/java/com/example/flixster/adaptors/MovieAdaptor.java
+++ app/src/main/java/com/example/flixster/adaptors/MovieAdaptor.java
@@ -1,19 +1,25 @@
 package com.example.flixster.adaptors;
 
 import android.content.Context;
+import android.content.Intent;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
+import com.example.flixster.DetailActivity;
 import com.example.flixster.R;
 import com.example.flixster.models.Movie;
+
+import org.parceler.Parcels;
 
 import java.util.List;
 
@@ -53,6 +59,7 @@ public class MovieAdaptor extends RecyclerView.Adapter<MovieAdaptor.ViewHolder>{
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
+        RelativeLayout container;
         TextView tvTtitle;
         TextView tvOverview;
         ImageView tvPoster;
@@ -62,12 +69,27 @@ public class MovieAdaptor extends RecyclerView.Adapter<MovieAdaptor.ViewHolder>{
             tvTtitle = itemView.findViewById(R.id.tvTitle);
             tvOverview = itemView.findViewById(R.id.tvOverview);
             tvPoster = itemView.findViewById(R.id.tvPoster);
+            container = itemView.findViewById(R.id.container);
         }
 
         public void bind(Movie movie) {
             tvTtitle.setText(movie.getTitle());
             tvOverview.setText(movie.getOverview());
             Glide.with(context).load(movie.getPosterPath()).into(tvPoster);
+
+            // register click listener on whole row
+            container.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v){
+                    // navigate to new activity on click
+                    //Toast.makeText(context, movie.getTitle(), Toast.LENGTH_SHORT).show();
+
+                    Intent i = new Intent(context, DetailActivity.class);
+                    //i.putExtra("title", movie.getTitle());
+                    i.putExtra("movie", Parcels.wrap(movie));
+                    context.startActivity(i);
+                }
+            });
         }
     }
 }
